492a08ad9b23ca22ad479d8192c134b2f9886173
yusuuf-dev <lucas2030garcia@gmail.com>
2026-02-26 11:12:35 +0000

    complete the algorithm of merge-insertion-sort using vectors

--- sort_algorithms/merge-insertion_sort.cpp
+++ sort_algorithms/merge-insertion_sort.cpp
@@ -23,7 +23,88 @@ void parsing(char **av, vector<int> &arr)
 
 void recursion_sort(vector<pair<int,int>>& pairs)
 {
+    if (pairs.size() <= 1)
+        return;
+    size_t mid = pairs.size() / 2;
+    
+    vector<pair<int,int>> left(pairs.begin(),pairs.begin()+mid);
+    vector<pair<int,int>> right(pairs.begin()+mid,pairs.end());
+    recursion_sort(left);
+    recursion_sort(right);
 
+    vector<pair<int,int>>::iterator it, it1 ,it2;
+    it = pairs.begin();
+    it1 = left.begin();
+    it2 = right.begin();
+    while (it1 != left.end() && it2 != right.end())
+    {
+        if (it1->first < it2->first)
+            *it++ = *it1++;
+        else
+            *it++ = *it2++;
+    }
+    if (it1 != left.end())
+    {
+        while (it1 != left.end() && it != pairs.end())
+            *it++ = *it1++;
+    }
+    if (it2 != right.end())
+    {
+        while (it2 != right.end() && it != pairs.end())
+            *it++ = *it2++;
+    }
+}
+
+bool compare(const pair<int,int>& elem,const pair<int,int>& tmp_elem)
+{
+    return elem.first < tmp_elem.first;
+}
+
+void insertion(vector<pair<int,int>>& pairs)
+{
+    vector<pair<int,int>> losers;
+
+    losers = pairs;
+
+    vector<int> jacobstal(22);
+    jacobstal[0] = 0;
+    jacobstal[1] = 1;
+    for (size_t i = 2; i < 20; i++)
+    {
+        jacobstal[i] = jacobstal[i - 1] + (jacobstal[i - 2] * 2);
+    }
+    
+    int currIndex = 0;
+    int jacobIndex = 2;
+    int oldIndex = 0;
+    while (currIndex < losers.size() - 1)
+    {
+        pair<int,int> tmp;
+        vector<pair<int,int>>::iterator target, insert_pos;
+        if (jacobstal[jacobIndex] == 1)
+        {
+            tmp.first = losers[currIndex].second;
+            tmp.second = losers[currIndex].first;
+            pairs.insert(pairs.begin(),tmp);
+        }
+        else 
+        {
+            oldIndex = currIndex;
+            if (jacobstal[jacobIndex] - 1 < losers.size())
+                currIndex = jacobstal[jacobIndex] - 1;
+            else
+                currIndex = losers.size() - 1;
+            for (int i = currIndex; oldIndex != i; --i)
+            {
+                tmp.first = losers[i].second;
+                tmp.second = losers[i].first;
+                target = find(pairs.begin(),pairs.end(),losers[i]);
+                insert_pos = lower_bound(pairs.begin(),target,tmp,compare);
+                pairs.insert(insert_pos,tmp);
+            }
+        }
+        ++jacobIndex;
+    }
 }
 
 void merge_insertion_sort(vector<int> &arr)
@@ -31,14 +112,23 @@ void merge_insertion_sort(vector<int> &arr)
     vector<pair<int,int>> pairs;
     int tmp;
     bool struggler = false;
+
     vector<pair<int,int>>::iterator it;
-    for (size_t i = 0; i < arr.size(); ++i)
+    for (size_t i = 0; i < arr.size();)
     {
         if (i + 1 < arr.size())
         {
             pair<int,int> p;
-            p.first = arr[i];
-            p.second = arr[i + 1];
+            if (arr[i] > arr[i+1])
+            {
+                p.first = arr[i];
+                p.second = arr[i + 1];
+            }
+            else
+            {
+                p.first = arr[i+1];
+                p.second = arr[i];
+            }
             pairs.push_back(p);
         }
         else
@@ -46,12 +136,18 @@ void merge_insertion_sort(vector<int> &arr)
             tmp = arr[i];
             struggler = true;
         }
+        i += 2;
     }
+    recursion_sort(pairs);
+
+    insertion(pairs);
+    
+    arr.clear();
     for (size_t i = 0; i < pairs.size(); i++)
     {
-        cout << '['<< pairs[i].first << ',' << pairs[i].second << ']' << '\t';
+        arr.push_back(pairs[i].first);
+        cout << pairs[i].first << (i < pairs.size()-1?',':'\n');
     }
-    cout << endl;
 }
 
 int main(int ac, char *av[])
